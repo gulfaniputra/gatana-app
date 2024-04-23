f4f5e439265621f35eb46d8a42b25ffbb153980f
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-23 20:02:27 +0800

    Break 'AnimeList' component into 'Anime'

--- src/App.jsx
+++ src/App.jsx
@@ -129,17 +129,27 @@ function AnimeList({ animes, onSelectedAnime }) {
   return (
     <ul className="list list-anime">
       {animes?.map((anime) => (
-        <li key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
-          <img src={anime.image} alt={`${anime.title} cover`} />
-          <h3>{anime.title}</h3>
-          <div>
-            <p>
-              <span>{anime.year}</span>
-            </p>
-          </div>
-        </li>
+        <Anime
+          key={anime.mal_id}
+          anime={anime}
+          onSelectedAnime={onSelectedAnime}
+        />
       ))}
     </ul>
+  );
+}
+
+function Anime({ anime, onSelectedAnime }) {
+  return (
+    <li onClick={() => onSelectedAnime(anime.mal_id)}>
+      <img src={anime.image} alt={`${anime.title} cover`} />
+      <h3>{anime.title}</h3>
+      <div>
+        <p>
+          <span>{anime.year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
