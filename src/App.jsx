5522838de66f310b1f9ff82ac6e60785cb6b6478
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-23 19:52:53 +0800

    Break 'ListBox' component into 'AnimeList'

--- src/App.jsx
+++ src/App.jsx
@@ -119,23 +119,27 @@ function ListBox({ animes, onSelectedAnime }) {
         {isOpen1 ? "–" : "+"}
       </button>
       {isOpen1 && (
-        <ul className="list list-anime">
-          {animes?.map((anime) => (
-            <li
-              key={anime.mal_id}
-              onClick={() => onSelectedAnime(anime.mal_id)}>
-              <img src={anime.image} alt={`${anime.title} cover`} />
-              <h3>{anime.title}</h3>
-              <div>
-                <p>
-                  <span>{anime.year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <AnimeList animes={animes} onSelectedAnime={onSelectedAnime} />
       )}
     </div>
+  );
+}
+
+function AnimeList({ animes, onSelectedAnime }) {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <li key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
+          <img src={anime.image} alt={`${anime.title} cover`} />
+          <h3>{anime.title}</h3>
+          <div>
+            <p>
+              <span>{anime.year}</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
